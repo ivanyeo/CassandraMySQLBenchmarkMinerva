6c3bec1d7f63454f7a6bae7a2995ce454fa1260b
Ivan <ivanyeo@cs.ucla.edu>
2014-05-16 22:50:10 -0400

    Add configuration file that query/ application should use

---
+++ tagteam/query/configurations.py
@@ -0,0 +1,13 @@
+"""
+TagTeam Configuration File
+
+This configuration file is required for the query/ application.
+
+All parts of the application should reference this configuration
+file for convenience so that all project settings can be
+set in a single location.
+"""
+
+POST_RETURN_LIMIT = 8
+
+
